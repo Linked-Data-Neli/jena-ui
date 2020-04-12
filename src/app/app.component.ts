7ab4e1bd21aaecf368bb18014ccecf2b91f5d225
AdrianAlucard <wartinator@gmail.com>
2020-04-11 21:31:38 -0400

    bootstrap and dropdown setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'jena-ui';
+  isStudentDetailsForm: boolean;
+  isCourseDetailsForm: boolean;
+
+  queryOptions = [];
+
+  ngOnInit() {
+    this.queryOptions.push("Student Details")
+    this.queryOptions.push("Course Details")
+  }
+
+
+  onSelect(query: string) {
+    // create form
+    console.log(query)
+  }
 }
